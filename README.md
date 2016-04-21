9595e03df4703480a46c7b26d31bf54cedd59224
Matt West <matt@mattdanger.net>
2016-04-21 15:54:45 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # phalcon-semaphore
+
 Simple semaphore component for Phalcon PHP
+
+## Requirements
+
+* Phalcon v1.4.x
+
+## Installation ##
+
+Install using Composer:
+
+```json
+{
+	"require": {
+		"mattdanger/phalcon-semaphore": "1.*"
+	}
+}
+```
+
+Create a database table `semaphore` with this schema:
+
+```sql
+CREATE TABLE `semaphore` (
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `value` varchar(255) DEFAULT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+```
+
+Then add the service:
+
+```php
+$di->set('semaphore', '\PhalconSemaphore\Semaphore');
+```
+
+If you want to use a different database table name you can initialize the service like this:
+
+```php
+$di->set('semaphore', function() use ($config){
+  return new \PhalconSemaphore\Semaphore('my_table_name');
+}, true);
+```
+
+## Usage
+
+```php
+
+$this->semaphore->run([Class], [method]);
+
+// Example without method parameters
+$this->semaphore->run('MyNamespace\Models\Stat', 'calculate');
+
+// Example with method parameters
+$this->semaphore->run('MyNamespace\Models\Stat', 'calculate', $arg1, $arg2, ...);
+```
---
+++ src/PhalconSemaphore/Semaphore.php
@@ -0,0 +1,65 @@
+<?php
+namespace PhalconSemaphore;
+
+use Phalcon\Mvc\User\Component;
+
+/**
+ * PhalconSemaphore
+ */
+class Semaphore extends Component
+{
+
+  public $table = '';
+
+  /**
+   * Constructor
+   *
+   * @param string $table
+   */
+  public function __construct($table = 'semaphore')
+  {
+
+    $this->table = $table;
+
+  }
+
+
+  /**
+   * Execute operation
+   *
+   * @param string $class
+   * @param string $method
+   * @param int $hours
+   * @param array $args
+   */
+  public function run($class, $method, $hours = 24, $args = array())
+  {
+
+    $name = "$class::$method";
+
+    if (class_exists($class)) {
+
+      try {
+
+        // If this semaphore has been run in the past $hours hours, return
+        $sql = 'SELECT * FROM ' . $this->table . ' WHERE name = ? AND timestamp >= DATE_SUB(NOW(), INTERVAL ? hour)';
+        if ($existing = $this->pdo->executeQuery($sql, array($name, $hours))) {
+          return;
+        }
+
+        // Perform operation
+        $results = call_user_func_array(array($class, $method), $args);
+
+        // Insert or update timestamp
+        $sql = 'INSERT INTO ' . $this->table . ' (`name`, `timestamp`) VALUES (?, NOW()) ON DUPLICATE KEY UPDATE timestamp = NOW()';
+        $this->pdo->executeQuery($sql, array($name));
+
+      } catch (Exception $e) {
+        // Error
+      }
+
+    }
+
+  }
+
+}
